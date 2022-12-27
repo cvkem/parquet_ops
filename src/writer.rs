ec6923c638546c8d9fbe5770007339ca32ca990e
Cees van Kemenade <cees.van.kemenade@gmail.com>
2022-12-27 09:31:22 +0100

    Added INT32-type  to writer

--- src/writer.rs
+++ src/writer.rs
@@ -3,7 +3,8 @@ use std::{
     io::Write,
     path::Path,
     slice::Iter,
-    sync::Arc
+    sync::Arc,
+    time::Instant
 };
 use parquet::{
     basic::{Compression, ConvertedType, Type as PhysicalType},
@@ -59,11 +60,14 @@ impl<W: Write> RowWriter::<W> {
     pub fn flush(&mut self) -> Result<()> {
         let mut row_group_writer = self.row_writer.next_row_group().unwrap();
 
+        let timer = Instant::now();
+
         for (idx, field) in self.schema.get_fields().iter().enumerate() {
 
             if let Some(mut col_writer) = row_group_writer.next_column().unwrap() {
                 match field.get_basic_info().converted_type() {
                     ConvertedType::INT_64 => write_i64_column(self.buffer.iter(), idx, &mut col_writer)?,
+                    ConvertedType::INT_32 => write_i32_column(self.buffer.iter(), idx, &mut col_writer)?,
                     ConvertedType::UTF8 => write_utf8_column(self.buffer.iter(), idx, &mut col_writer)?,
                     ConvertedType::TIMESTAMP_MILLIS => {
     
@@ -72,6 +76,7 @@ impl<W: Write> RowWriter::<W> {
                     ConvertedType::NONE => {
                         match field.get_physical_type() {
                             PhysicalType::INT64 => write_i64_column(self.buffer.iter(), idx, &mut col_writer)?,
+                            PhysicalType::INT32 => write_i32_column(self.buffer.iter(), idx, &mut col_writer)?,
                             _ => {
                                 panic!("Column {idx}: Unknown Pysical-type {:?}", field.get_physical_type());
                             }
@@ -79,13 +84,16 @@ impl<W: Write> RowWriter::<W> {
                     },
                     // some more types need to be implemented
                     _ => panic!("Column {idx}: Unknown Converted-type {:?}", field.get_basic_info().converted_type())
-                }    
+                }
+                // ensure the col_writer is closed, however, end of block possibly does close it automatic.
+                col_writer.close();    
             } else {
                 panic!("Could not find a column-writer for column {idx} containing {:#?}", field)
             }
-            
- 
         }
+        row_group_writer.close();
+
+        println!("Flushing the row-group takes {:?}", timer.elapsed());
         Ok(())
     }
 
@@ -137,6 +145,19 @@ fn write_i64_column(rows: Iter<Row>,  idx: usize, col_writer: &mut SerializedCol
     Ok(())
 }
 
+fn write_i32_column(rows: Iter<Row>,  idx: usize, col_writer: &mut SerializedColumnWriter) -> Result<()> {
+    let column: Vec<i32> = rows
+        .map(|row| row.get_int(idx).unwrap() )
+        .collect();
+    let the_min = column.iter().min().unwrap();
+    let the_max = column.iter().max().unwrap();
+
+    col_writer
+        .typed::<Int32Type>()
+        .write_batch_with_statistics(&column, None, None, Some(&the_min), Some(&the_max), None)?;
+    Ok(())
+}
+
 
 
 fn write_utf8_column(rows: Iter<Row>, idx: usize, col_writer: &mut SerializedColumnWriter) -> Result<()> {
@@ -154,52 +175,91 @@ fn write_utf8_column(rows: Iter<Row>, idx: usize, col_writer: &mut SerializedCol
 
 
 
-// #[cfg(test)]
-// pub mod tests {
+#[cfg(test)]
+pub mod tests {
 
-//     use std::{
-//         path::Path,
-//         sync::Arc};
-//     use parquet::{
-//         basic::Compression,
-//         data_type::{Int32Type, Int64Type, ByteArrayType, ByteArray},
-//         file::{
-//             properties::WriterProperties,
-//             writer::SerializedFileWriter,
-//             reader::SerializedFileReader,
-//             reader::FileReader
-//         },
-//         record::{Row, 
-//             api::{Field, make_row}},
-//         schema::{parser::parse_message_type,
-//             types::Type}
-//     };
-//     use crate::RowWriter;
+    use std::{
+        fs::File,
+        path::Path,
+        sync::Arc};
+    use parquet::{
+        basic::Compression,
+        data_type::{Int32Type, Int64Type, ByteArrayType, ByteArray},
+        file::{
+            properties::WriterProperties,
+            writer::{
+                SerializedFileWriter,
+                SerializedRowGroupWriter},
+            reader::{
+                SerializedFileReader,
+                FileReader}
+        },
+        record::{Row, Field},
+//            api::{Field, make_row}},
+        schema::{parser::parse_message_type,
+            types::Type}
+    };
+    use parquet_derive::ParquetRecordWriter;
+    use crate::RowWriter;
+
+    //#[derive(Debug)]
+    // I expect some kind of feature of arrow-parquet is needed to make this work.
+    #[derive(ParquetRecordWriter)]
+    struct TestAccount<'a> {
+        pub id: i64,
+        pub account: &'a str
+    }
 
 
-//     #[test]
-//     fn test_write_parquet() {
-//         const MESSAGE_TYPE: &str = "
-//         message schema {
-//             REQUIRED INT64 id;
-//             REQUIRED BINARY account (UTF8);
-//         ";
-//         let input_tuples = vec![(1_i64, "Hello"), (2_i64, "World")];
+    // this is not the right test as I switch to example code
+    #[test]
+    fn test_write_parquet() {
+        const MESSAGE_TYPE: &str = "
+        message schema {
+            REQUIRED INT64 id;
+            REQUIRED BINARY account (UTF8);
+        ";
+        // let input_tuples = vec![(1_i64, "Hello"), (2_i64, "World")];
 
-//         let tuple_to_row = |(id, account)|  vec![("id", Field.Long(id)), ("account", Field.Str(account))]; 
-//         let input_rows = make_row(input_tuples.into_iter().map(tuple_to_row).collect()); 
+        // let tuple_to_row = |(id, account)|  vec![("id", Field.Long(id)), ("account", Field.Str(account))]; 
+        // let input_rows = make_row(input_tuples.into_iter().map(tuple_to_row).collect()); 
+        let test_account_data = vec![
+            TestAccount {
+                id: 1,
+                account: "Hello"
+            },
+            TestAccount {
+                id: 2,
+                account: "World"
+            }
+        ];
+        println!("Original data: {:#?}", test_account_data);
+
+        let path = Path::new("/tmp/test_write_parquet.parquet");
+        let schema = Arc::new(parse_message_type(MESSAGE_TYPE).unwrap());
+        let generated_schema = test_account_data.as_slice().schema().unwrap();
+
+        let props = Arc::new(WriterProperties::builder()
+            .set_compression(Compression::SNAPPY)
+            .build());
+//        let row_writer = RowWriter::new(path, schema, 10).unwrap();
+        let file = fs::File::create(&path).unwrap();
+        let mut writer =
+            SerializedFileWriter::new(file, generated_schema, props).unwrap();
 
 
-//         let path = Path::new("/tmp/test_write_parquet.parquet");
-//         let schema = Arc::new(parse_message_type(MESSAGE_TYPE).unwrap());
+        println!(" row_data= {:#?}", generated_schema);
 
-//         let row_writer = RowWriter::new(path, schema, 10).unwrap();
+        // for row in row_data {
+        //     row_writer.append_row(row);
+        // }
+        let mut row_group = row_writer.next_row_group().unwrap();
+        test_account_data.as_slice().write_to_row_group(&mut row_group).unwrap();
+        row_group.close().unwrap();
 
-//         for row in input_rows {
-//             row_writer.append_row(row);
-//         }
-//         row_writer.close();
 
-//     }
+        row_writer.close().unwrap();
 
-// }
+    }
+
+}
