93d2b5b955980591b6f2f9783e3895a17fbaa9e4
Cees van Kemenade <cees.van.kemenade@gmail.com>
2023-05-02 19:33:08 +0200

    Moved partitioning code to a separate module

--- src/sort.rs
+++ src/sort.rs
@@ -1,13 +1,13 @@
 use itertools::Itertools;
 use parquet::record::{Row, RowAccessor};
 use std::{cmp::Ordering, sync::Arc};
-use crate::{rowiterext::read_row_sample};
 use super::rowiterext::RowIterExt;
 use super::rowwritebuffer::RowWriteBuffer;
 
 mod parquet_key;
-
 use parquet_key::{ParquetKey, sort_multistage_typed};
+mod partition;
+use partition::partitioning;
 
 const MAX_SORT_BLOCK: u64 = 1_000_000; //10_000  // 1 in REPORT_APPEND_STEP rows is reported on the console.
 
@@ -46,22 +46,12 @@ pub fn sort_multistage(
 
     let parquet_key = ParquetKey::new(sort_field_name.to_owned(), Arc::clone(&schema));
 
-    let single_column_message_type = parquet_key.get_partition_message_schema();
-                                                   // row 'account' should be flexible.
-    let mut sample = read_row_sample(input_path, 1000, &single_column_message_type);
-
-    sample.sort_unstable_by(parquet_key.get_partition_compare_fn());
-    let num_part = 3;
-    let step_size = sample.len() / (num_part - 1);
-    let partition = sample
-        .into_iter()
-        .batching(|it| it.skip(step_size - 1).next()) // deterministic step_by that always skips step_size -1 fields before taking the first value
-        //        .step_by(step_size)
-        .collect::<Vec<_>>();
+    let partition = partitioning(input_path, &parquet_key, 3);
 
     let base_path_stage_1 = sorted_path.to_owned() + ".intermediate";
 
-    let interm_paths: Vec<_> = (0..num_part)
+    let num_row_writer = partition.len()+1;  // Last row_writer is needed to store the tail (N partitions result in N+1 segments.
+    let interm_paths: Vec<_> = (0..num_row_writer)
         .map(|i| format!("{}-{}", base_path_stage_1, i))
         .collect();
     let mut row_writer: Vec<_> = interm_paths
---
+++ src/sort/partition.rs
@@ -0,0 +1,20 @@
+use itertools::Itertools;
+use crate::rowiterext::read_row_sample;
+use parquet::record::Row;
+use super::parquet_key::{ParquetKey, sort_multistage_typed};
+
+/// Get a partition over a large dataset
+pub fn partitioning(input_path: &str, parquet_key: &ParquetKey, num_partition: usize) -> Vec<Row> {
+    let single_column_message_type = parquet_key.get_partition_message_schema();
+    // row 'account' should be flexible.
+    let mut sample = read_row_sample(input_path, 1000, &single_column_message_type);
+
+    sample.sort_unstable_by(parquet_key.get_partition_compare_fn());
+    let step_size = sample.len() / (num_partition - 1);
+    let partition = sample
+        .into_iter()
+        .batching(|it| it.skip(step_size - 1).next()) // deterministic step_by that always skips step_size -1 fields before taking the first value
+        .collect::<Vec<_>>();
+
+    partition    
+}
