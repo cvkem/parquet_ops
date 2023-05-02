eeadd6a12e763c3e1055027bcdc13a12c7873077
Cees van Kemenade <cees.van.kemenade@gmail.com>
2023-05-02 17:32:43 +0200

    Refactor: Move ParquetKey to separate module

--- src/sort.rs
+++ src/sort.rs
@@ -1,13 +1,13 @@
 use itertools::Itertools;
-use parquet::{
-    basic::Type as PhysType,
-    record::{Row, RowAccessor},
-    schema::types::Type,
-};
+use parquet::record::{Row, RowAccessor};
 use std::{cmp::Ordering, sync::Arc};
-use crate::{rowiterext::read_row_sample, find_field};
+use crate::{rowiterext::read_row_sample};
 use super::rowiterext::RowIterExt;
 use super::rowwritebuffer::RowWriteBuffer;
+
+mod parquet_key;
+
+use parquet_key::{ParquetKey, sort_multistage_typed};
 
 const MAX_SORT_BLOCK: u64 = 1_000_000; //10_000  // 1 in REPORT_APPEND_STEP rows is reported on the console.
 
@@ -32,73 +32,6 @@ pub fn sort(input_path: &str, sorted_path: &str, comparator: fn(&Row, &Row) -> O
     row_writer.close();
 }
 
-pub trait sort_multistage_typed {
-    fn get_partition_compare_fn(&self) -> Box<dyn Fn(&Row, &Row) -> Ordering>;
-    fn get_record_compare_fn(&self) -> Box<dyn Fn(&Row, &Row) -> Ordering>;
-    fn get_partition_filter_fn(&self, partition_row: &Row) -> Box<dyn Fn(&Row) -> bool>;
-    fn get_partition_message_schema(&self) -> String;
-}
-
-struct ParquetKey {
-    name: String,
-    sort_col: usize,
-    phys_type: PhysType,
-}
-
-impl ParquetKey {
-    fn new(name: String, schema: Arc<Type>) -> Self {
-        let (sort_col, tpe) = find_field(schema, &name);
-        let phys_type = tpe.get_physical_type();
-
-        Self{name, sort_col, phys_type}
-    }
-}
-
-impl sort_multistage_typed for ParquetKey {
-    fn get_partition_compare_fn(&self) -> Box<dyn Fn(&Row, &Row) -> Ordering> {
-        match self.phys_type {
-            PhysType::INT64 => Box::new(|left: &Row, right: &Row| left.get_long(0).unwrap().cmp(&right.get_long(0).unwrap())),
-            PhysType::INT32 => Box::new(|left: &Row, right: &Row| left.get_int(0).unwrap().cmp(&right.get_int(0).unwrap())),
-            other =>  panic!("columns of type '{other}' are not supported (yet)!")
-        }
-    }
-
-    fn get_record_compare_fn(&self) -> Box<dyn Fn(&Row, &Row) -> Ordering> {
-        let col = self.sort_col;
-        match self.phys_type {
-            PhysType::INT64 => Box::new(move |left: &Row, right: &Row| left.get_long(col).unwrap().cmp(&right.get_long(col).unwrap())),
-            PhysType::INT32 => Box::new(move |left: &Row, right: &Row| left.get_int(col).unwrap().cmp(&right.get_int(col).unwrap())),
-            other =>  panic!("columns of type '{other}' are not supported (yet)!")
-        }
-    }
-
-    fn get_partition_filter_fn(&self, partition_row: &Row) -> Box<dyn Fn(&Row) -> bool> {
-        match self.phys_type {
-            PhysType::INT64 => {
-                let col = self.sort_col;
-                let upper_bound = partition_row.get_long(col).unwrap();
-                Box::new(move|row: &Row| row.get_long(col).unwrap() <= upper_bound)},
-            PhysType::INT32 => {
-                let col = self.sort_col;
-                let upper_bound = partition_row.get_int(col).unwrap();
-                Box::new(move|row: &Row| row.get_int(col).unwrap() <= upper_bound)},
-            other =>  panic!("columns of type '{other}' are not supported (yet)!")
-        }
-    }
-
-    fn get_partition_message_schema(&self) -> String {
-        let type_label = match self.phys_type {
-            PhysType::INT64 => "INT64",
-            PhysType::INT32 => "INT32",
-            other =>  panic!("columns of type '{other}' are not supported (yet)!")
-        };
-        
-        format!("
-        message schema {{
-          REQUIRED {type_label} {};
-        }}", self.name)
-    }
-}
 
 /// Sort the input in two passes. The first pass returns a file with sorted row-groups. In the second pass these row-groups are merged.
 pub fn sort_multistage(
---
+++ src/sort/parquet_key.rs
@@ -0,0 +1,76 @@
+use parquet::{
+    basic::Type as PhysType,
+    record::{Row, RowAccessor},
+    schema::types::Type,
+};
+use std::{cmp::Ordering, sync::Arc};
+use crate::find_field;
+
+
+pub trait sort_multistage_typed {
+    fn get_partition_compare_fn(&self) -> Box<dyn Fn(&Row, &Row) -> Ordering>;
+    fn get_record_compare_fn(&self) -> Box<dyn Fn(&Row, &Row) -> Ordering>;
+    fn get_partition_filter_fn(&self, partition_row: &Row) -> Box<dyn Fn(&Row) -> bool>;
+    fn get_partition_message_schema(&self) -> String;
+}
+
+pub struct ParquetKey {
+    name: String,
+    sort_col: usize,
+    phys_type: PhysType,
+}
+
+impl ParquetKey {
+    pub fn new(name: String, schema: Arc<Type>) -> Self {
+        let (sort_col, tpe) = find_field(schema, &name);
+        let phys_type = tpe.get_physical_type();
+
+        Self{name, sort_col, phys_type}
+    }
+}
+
+impl sort_multistage_typed for ParquetKey {
+    fn get_partition_compare_fn(&self) -> Box<dyn Fn(&Row, &Row) -> Ordering> {
+        match self.phys_type {
+            PhysType::INT64 => Box::new(|left: &Row, right: &Row| left.get_long(0).unwrap().cmp(&right.get_long(0).unwrap())),
+            PhysType::INT32 => Box::new(|left: &Row, right: &Row| left.get_int(0).unwrap().cmp(&right.get_int(0).unwrap())),
+            other =>  panic!("columns of type '{other}' are not supported (yet)!")
+        }
+    }
+
+    fn get_record_compare_fn(&self) -> Box<dyn Fn(&Row, &Row) -> Ordering> {
+        let col = self.sort_col;
+        match self.phys_type {
+            PhysType::INT64 => Box::new(move |left: &Row, right: &Row| left.get_long(col).unwrap().cmp(&right.get_long(col).unwrap())),
+            PhysType::INT32 => Box::new(move |left: &Row, right: &Row| left.get_int(col).unwrap().cmp(&right.get_int(col).unwrap())),
+            other =>  panic!("columns of type '{other}' are not supported (yet)!")
+        }
+    }
+
+    fn get_partition_filter_fn(&self, partition_row: &Row) -> Box<dyn Fn(&Row) -> bool> {
+        match self.phys_type {
+            PhysType::INT64 => {
+                let col = self.sort_col;
+                let upper_bound = partition_row.get_long(col).unwrap();
+                Box::new(move|row: &Row| row.get_long(col).unwrap() <= upper_bound)},
+            PhysType::INT32 => {
+                let col = self.sort_col;
+                let upper_bound = partition_row.get_int(col).unwrap();
+                Box::new(move|row: &Row| row.get_int(col).unwrap() <= upper_bound)},
+            other =>  panic!("columns of type '{other}' are not supported (yet)!")
+        }
+    }
+
+    fn get_partition_message_schema(&self) -> String {
+        let type_label = match self.phys_type {
+            PhysType::INT64 => "INT64",
+            PhysType::INT32 => "INT32",
+            other =>  panic!("columns of type '{other}' are not supported (yet)!")
+        };
+        
+        format!("
+        message schema {{
+          REQUIRED {type_label} {};
+        }}", self.name)
+    }
+}
